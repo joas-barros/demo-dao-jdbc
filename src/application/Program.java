0ddcd8300333216987d7c6671ea73186f243c4d8
joas-barros <joas.barros14@hotmail.com>
2024-06-11 20:18:25 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,8 @@ public class Program {
         Department obj = new Department(1, "books");
 
         Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
 
