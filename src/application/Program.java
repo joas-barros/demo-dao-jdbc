36d4bc8f7641c7c8bcac5b4e1ea2aee77af96577
joas-barros <joas.barros14@hotmail.com>
2024-06-12 10:23:14 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,14 +10,10 @@ import java.util.Date;
 public class Program {
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "books");
-
-        Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000, obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        System.out.println(seller);
+        Seller seller = sellerDao.findById(3);
 
-        System.out.println(obj);
+        System.out.println(seller);
     }
 }
